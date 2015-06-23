48c9a98d6d5374447542b42a6063cdb81ad84fae
danthemanzx <dan.the.man.zx@gmail.com>
2015-06-23 10:40:46 -0500

    Spigot 1.8.6

--- spigotServer_1.7.9/Start.bat
+++
@@ -1,6 +0,0 @@
-@Echo OFF
-Title Mc Bukkit Server
-Color F
-
-java -Xms512M -Xmx1024M -jar spigot.jar
-pause
---
+++ spigotServer_1.7.X/Start.bat
@@ -0,0 +1,6 @@
+@Echo OFF
+Title Mc Bukkit Server
+Color F
+
+java -Xms512M -Xmx1024M -jar spigot_server.jar
+pause
