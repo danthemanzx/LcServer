9227ec1fe83a2b39f25394c73f4ec9af05432096
danthemanzx <dan.the.man.zx@gmail.com>
2015-06-24 15:44:43 -0500

    Bukkit Switch

---
+++ bukkitServer_1.8.6/Start.bat
@@ -0,0 +1,6 @@
+@Echo OFF
+Title Mc Bukkit Server
+Color F
+
+java -Xms1G -Xmx2G -XX:MaxPermSize=128M -jar craftbukkit-1.8.7.jar
+pause
--- spigotServer_1.8.6/Start.bat
+++
@@ -1,6 +0,0 @@
-@Echo OFF
-Title Mc Bukkit Server
-Color F
-
-java -Xms1G -Xmx2G -XX:MaxPermSize=128M -jar spigot_server.jar
-pause
